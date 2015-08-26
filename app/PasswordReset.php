e7eefaecab34ff191a6cf75b62ff6ccb928e0e78
Naveen <naveen@asareri.com>
2015-08-26 12:08:24 +0530

    test

--- app/PasswordReset.php
+++ app/PasswordReset.php
@@ -6,17 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class PasswordReset extends Model {
 
-  /**
-  * The database table used by the model.
-  *
-  * @var string
-  */
-  protected $table = 'password_resets';
+      /**
+      * The database table used by the model.
+      *
+      * @var string
+      */
+      protected $table = 'password_resets';
 
-  /**
-  * The attributes that are mass assignable.
-  *
-  * @var array
-  */
-  protected $fillable = array('email', 'token');
+      /**
+      * The attributes that are mass assignable.
+      *
+      * @var array
+      */
+      protected $fillable = array('email', 'token');
 }
